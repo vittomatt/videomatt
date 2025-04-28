33c5f148937cc3913c148baf034c0d45a2803a17
Vitto Matt <victorio.matteucci@gmail.com>
2025-04-28 12:09:27 -0300

    add husky

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,13 @@
 module.exports = {
     preset: 'ts-jest',
+    roots: ['<rootDir>/src'],
     testEnvironment: 'node',
     moduleNameMapper: {
         '^@videomatt/(.*)$': '<rootDir>/src/videomatt/$1',
     },
     modulePaths: ['<rootDir>/src'],
     setupFiles: ['reflect-metadata'],
+    testMatch: [
+        '**/?(*.)+(test).+(ts|js)'
+    ]
 };
